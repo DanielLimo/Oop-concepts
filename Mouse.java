3d8cad32a9edada4a9c94245eee2b06d587d5cb6
DanielLimo <137399152+DanielLimo@users.noreply.github.com>
2023-06-28 23:03:03 +0300

    Add files via upload

---
+++ Mouse.java
@@ -0,0 +1,16 @@
+
+public class Mouse  {
+	public static void LeftClick() {
+   	 System.out.println(" Click Click Bang!!!");
+    }
+    public static void rightClick() {
+   	 System.out.println(" Click Click Bang!!!");
+    }
+    public static void scrollUp() {
+    	System.out.println(" Scrolled Up");
+    	
+    }
+    public static void scrollDown() {
+    	System.out.println(" Scrolled Down");
+}
+}
