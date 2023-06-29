9c94fa9b4541a2193f0950ef949b43cf7d00e344
DanielLimo <137399152+DanielLimo@users.noreply.github.com>
2023-06-29 09:05:35 +0300

    Add files via upload

---
+++ Inheritance/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   Mouse1 m1=new Mouse1();
+    
+    m1.LeftClick();
+    m1.rightClick();
+    m1.scrollDown();
+    m1.scrollUp();
+	}
+
+}
