5730aedcfe79812d7185c887d9845624b91ebc10
DanielLimo <137399152+DanielLimo@users.noreply.github.com>
2023-06-29 09:00:34 +0300

    Add files via upload

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Student s=new Student();
+  s.setName("Daniel");
+  s.setAge(20);
+  System.out.println(s.getName());
+  System.out.println(s.getAge());
+	}
+ 
+
+}
